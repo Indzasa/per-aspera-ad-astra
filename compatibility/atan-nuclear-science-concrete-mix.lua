a3488c063158805e473b0f4458597c6d00fd0ff3
indzasa <indzasa@gmail.com>
2025-06-14 00:32:06 +0300

    fixed nuclear science productivity

--- compatibility/atan-nuclear-science-concrete-mix.lua
+++ compatibility/atan-nuclear-science-concrete-mix.lua
@@ -7,7 +7,8 @@ local frep = require("__fdsl__.lib.recipe")
 local function change_concrete_to_barrels(recipe_to_change)
 	frep.replace_ingredient(recipe_to_change, "concrete", "concrete-mix-barrel")
 	frep.modify_ingredient(recipe_to_change, "concrete-mix-barrel", {amount = 4})
-	frep.modify_result(recipe_to_change, "barrel", {amount = 24, ignored_by_productivity = 24})	
+	frep.modify_result(recipe_to_change, "barrel", {amount = 24})
+	frep.modify_result(recipe_to_change, "barrel", {ignored_by_productivity = 24})		
 end
 
 change_concrete_to_barrels("nuclear-science-pack")
